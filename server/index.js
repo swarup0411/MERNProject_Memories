435b5c463c8184b20e392cbdc5b84f4189ab5ce0
Swarup Satpathy <swarup0411@gmail.com>
2022-06-16 21:09:31 +0530

    redux, form, styling, create Post changes

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ import postRoutes from './routes/posts.js';
 const app = express();
 const sanket = 100;
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://root:root123@cluster0.zqbus.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
